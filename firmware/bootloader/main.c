7d590807cabf8d3694b804b7a01e631275ee78dd
jdeguire <me100313@gmail.com>
2010-11-06 03:01:04 -0400

    Blinking an LED: it's like "Hello, world!" for microcontrollers.
    Just got a breadboard together and used this to verify that it works.  It does!

--- firmware/bootloader/main.c
+++ firmware/bootloader/main.c
@@ -1,4 +1,16 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
 int main(void)
 {
-	return 0;
+	// LED on PC5
+	DDRC |= (1 << DD5);       // 1 sets pin to output
+	PORTC |= (1 << PC5);     // start with output low
+
+	while(1)
+	{
+		PINC |= (1 << PINC5); // set this to toggle PORTxn bit; read to get pin state
+		_delay_ms(1000);
+	}
 }
