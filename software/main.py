ce0dc557296db7058e9e20478426f1ff70b6e396
jdeguire <me100313@gmail.com>
2011-08-20 03:10:16 -0400

    Dusting off the repo with some PySide.
    Adding a main.py so I can keep track of what I'm doing with PySide while I'm learning it.

---
+++ software/main.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+
+"""
+main.py
+
+Will eventually become the entry point for the controller app.
+Now, it's just a way for me to experiment with PySide.
+"""
+
+import sys
+import os
+import pjcbootloader
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+
+class UpdatePage:
+    
+    def __init__(self, parent = None):
+        # create our main dialog box and set its title
+        self.dialog = QDialog(parent)
+        self.dialog.setWindowTitle('Update Box')
+
+        # widgets in the dialog box
+        self.fileline = QLineEdit('Select hex file...')
+        self.browsebutton = QPushButton('Browse...')
+        self.progress = QProgressBar()
+        self.startbutton = QPushButton('Start')
+
+        self.progress.setMinimum(0)
+
+        # so our file dialog remembers where we last were (default to home directory)
+        self.lasthexdir = os.path.expanduser('~')
+
+        # put the widgets into a vertical layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.fileline)
+        layout.addWidget(self.browsebutton)
+        layout.addWidget(self.progress)
+        layout.addWidget(self.startbutton)
+        self.dialog.setLayout(layout)   # can also pass 'dialog' to the layout's constructor
+
+        # connect signals from buttons to slots
+        self.browsebutton.clicked.connect(self.browseForHexFile)
+        self.startbutton.clicked.connect(self.doFirmwareUpdate)
+
+    def browseForHexFile(self):
+        hexfile = QFileDialog.getOpenFileName(self.dialog, 'Select hex file', self.lasthexdir,
+                                              'Intel hex files (*.hex);;All Files (*)')
+
+        if hexfile[0] != '':
+            self.fileline.setText(hexfile[0])
+            self.lasthexdir = os.path.dirname(hexfile[0])
+
+    def doFirmwareUpdate(self):
+        # later we'll need to create a selection box for the serial device
+        pjc = pjcbootloader.PJCBootloader("/dev/ttyUSB0")
+        self.progress.reset()
+        
+        # not final, doesn't handle exceptions and stuff
+        if pjc.getBootloaderVersion() >= 0:
+            if pjc.parseFile(self.fileline.text()):
+                pjc.getMaxPages()
+                pjc.getPageSize()
+                
+                numpages = pjc.getFileNumPages()
+                self.progress.setMaximum(numpages)
+
+                print 'File CRC: ' + hex(pjc.calculateFileCRC())
+                print 'File pages: ' + str(numpages) + '\n'
+                
+                print 'Erasing old app...'
+                pjc.eraseApp()
+
+                print 'Loading new app:',
+
+                for i in range(numpages):
+                    pageresult = pjc.programPage(i)
+
+                    if 0 == pageresult:
+                        self.progress.setvalue(i + 1)
+                    else:
+                        print '\nFailed to program page ' + str(i)
+                        break;
+
+                if 0 == pageresult:
+                    pjc.writeCRC()
+                    print 'Update complete!'
+                else:
+                    print 'Update returned error code ' + str(pageresult)
+            else:
+                print 'File parse failed'
+        else:
+            print 'Could not communicate with bootloader'
+
+
+    def show(self):
+        self.dialog.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    page = UpdatePage()
+    page.show()
+    
+    sys.exit(app.exec_())
+        
