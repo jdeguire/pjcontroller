4c89285b5f524df2246b17bbf30c253eac4c174c
jdeguire <me100313@gmail.com>
2012-06-17 17:42:50 -0400

    Moved serial combo box and buttons into MainWindow.

--- software/main.py
+++ software/main.py
@@ -9,22 +9,57 @@ The entry point for the controller app.
 import sys
 import pages
 import commthread
+from PySide import QtCore
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-class MainWindow:
+class MainWindow(QDialog):
+    serialopenclicked = QtCore.Signal(str)
+
     def __init__(self):
+        QDialog.__init__(self)
+        self.setWindowTitle("Test App")
+
         self.tabwidget = QTabWidget()
         self.updatepage = pages.UpdatePage()
 
-        self.tabwidget.setWindowTitle("Test App")
+        self.serialcombo = QComboBox()
+        self.serialopenbutton = QPushButton('Open')
+        self.serialrefreshbutton = QPushButton('Refresh')
+
+        self.serialcombo.setInsertPolicy(QComboBox.InsertPolicy.InsertAtTop)
+        self.serialcombo.setEditable(True)
+
         self.tabwidget.addTab(self.updatepage, "Update")
 
-    def show(self):
-        self.tabwidget.show()
+        # alias existing signals so they're easier to access externally
+        self.serialrefreshclicked = self.serialrefreshbutton.clicked
 
+        # set up our control layout
+        self.vbox = QVBoxLayout(self)
+        self.serialhbox = QHBoxLayout()
+        self.vbox.addLayout(self.serialhbox)
+        self.serialhbox.addWidget(self.serialcombo)
+        self.serialhbox.addWidget(self.serialopenbutton)
+        self.serialhbox.addWidget(self.serialrefreshbutton)
+        self.vbox.addWidget(self.tabwidget)
+
+        # connect signals to internal slots
+        self.serialopenbutton.clicked.connect(self.openSerial)
+
+    @QtCore.Slot(str)
     def printText(self, text):
         print text
+
+    @QtCore.Slot()
+    def openSerial(self):
+        self.serialopenclicked.emit(self.serialcombo.currentText())
+
+    @QtCore.Slot(list)
+    def setSerialPortChoices(self, portlist):
+        self.serialcombo.clear()
+        self.serialcombo.addItems(portlist)
+
 
 def main(argv = None):
     if argv is None:
@@ -36,11 +71,11 @@ def main(argv = None):
 
     # connect signals and slots between UI and Comm Thread
     mainwindow.updatepage.updatestartclicked.connect(comm.doFirmwareUpdate)
-    mainwindow.updatepage.serialopenclicked.connect(comm.openSerialPort)
-    mainwindow.updatepage.serialrefreshclicked.connect(comm.enumerateSerialPorts)
+    mainwindow.serialopenclicked.connect(comm.openSerialPort)
+    mainwindow.serialrefreshclicked.connect(comm.enumerateSerialPorts)
     comm.updateprogressed.connect(mainwindow.updatepage.setUpdateProgress)
     comm.updatecompleted.connect(mainwindow.updatepage.endUpdate)
-    comm.serialenumerated.connect(mainwindow.updatepage.setSerialPortChoices)
+    comm.serialenumerated.connect(mainwindow.setSerialPortChoices)
     comm.newtextmessage.connect(mainwindow.printText)
 
     comm.start()
--- software/pages.py
+++ software/pages.py
@@ -16,7 +16,6 @@ from PySide.QtGui import *
 class PageBase(QDialog):
     """Base class for the pages that can appear in the main tab window.
     """
-    
     def __init__(self):
         QDialog.__init__(self)
 
@@ -24,49 +23,32 @@ class PageBase(QDialog):
 class UpdatePage(PageBase):
     """The page used for performing firmware updates to the device.
     """
-
     # new signals have to be declared out here, something the docs aren't very explicit about
     updatestartclicked = QtCore.Signal(str)
-    serialopenclicked = QtCore.Signal(str)
 
     def __init__(self):
         PageBase.__init__(self)
 
         # widgets in the dialog box
-        self.serialcombo = QComboBox()
-        self.serialopenbutton = QPushButton('Open')
-        self.serialrefreshbutton = QPushButton('Refresh')
         self.fileline = QLineEdit('Select hex file...')
         self.browsebutton = QPushButton('Browse...')
-        self.progress = QProgressBar()
         self.startbutton = QPushButton('Start')
-
+        self.progress = QProgressBar()
         self.progress.setRange(0, 100)
-        self.serialcombo.setInsertPolicy(QComboBox.InsertPolicy.InsertAtTop)
-        self.serialcombo.setEditable(True)
-
-        # alias existing signals so they're easier to access externally
-        self.serialrefreshclicked = self.serialrefreshbutton.clicked
 
         # so our file dialog remembers where we last were (default to home directory)
         self.lasthexdir = os.path.expanduser('~')
 
         # put the widgets into a vertical layout
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.serialcombo)
-        self.serlayout = QHBoxLayout()
-        self.layout.addLayout(self.serlayout)
-        self.serlayout.addWidget(self.serialopenbutton)
-        self.serlayout.addWidget(self.serialrefreshbutton)
-        self.layout.addWidget(self.fileline)
-        self.layout.addWidget(self.browsebutton)
-        self.layout.addWidget(self.progress)
-        self.layout.addWidget(self.startbutton)
+        self.vbox = QVBoxLayout(self)
+        self.vbox.addWidget(self.fileline)
+        self.vbox.addWidget(self.browsebutton)
+        self.vbox.addWidget(self.progress)
+        self.vbox.addWidget(self.startbutton)
 
         # connect signals to internal slots
         self.browsebutton.clicked.connect(self.browseForHexFile)
         self.startbutton.clicked.connect(self.startNewUpdate)
-        self.serialopenbutton.clicked.connect(self.openSerial)
 
     @QtCore.Slot()
     def browseForHexFile(self):
@@ -81,15 +63,6 @@ class UpdatePage(PageBase):
     def startNewUpdate(self):
         self.progress.reset()
         self.updatestartclicked.emit(self.fileline.text())
-
-    @QtCore.Slot()
-    def openSerial(self):
-        self.serialopenclicked.emit(self.serialcombo.currentText())
-
-    @QtCore.Slot(list)
-    def setSerialPortChoices(self, portlist):
-        self.serialcombo.clear()
-        self.serialcombo.addItems(portlist)
 
     @QtCore.Slot(int)
     def setUpdateProgress(self, prog):
